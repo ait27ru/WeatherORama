90954db48780ceeb648eec00ef9c1a80e10c22be
ait27ru <ait27ru@gmail.com>
2017-06-13 22:43:43 +1200

    Added CurrentConditionsDisplay and tied everything up.

---
+++ WeatherORama.WinConsole/Display/CurrentConditionsDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WeatherORama.WinConsole.Interface;
+
+namespace WeatherORama.WinConsole.Display
+{
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private readonly ISubject _weatherData;
+        private double _humidity;
+        private double _temperature;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            _weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Current conditions: {_temperature}°C degrees and {_humidity}% humidity.");
+        }
+
+        public void Update(double temp, double humidity, double pressure)
+        {
+            _temperature = temp;
+            _humidity = humidity;
+            Display();
+        }
+    }
+}
--- WeatherORama.WinConsole/Program.cs
+++ WeatherORama.WinConsole/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace WeatherORama.WinConsole
+﻿using System;
+using WeatherORama.WinConsole.Display;
+
+namespace WeatherORama.WinConsole
 {
     internal class Program
     {
         private static void Main()
         {
+            var weatherData = new WeatherData();
+            var currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+            weatherData.SetMeasurement(10, 90, 80);
+            weatherData.SetMeasurement(12, 85, 75);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
